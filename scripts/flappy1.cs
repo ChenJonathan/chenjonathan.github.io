457d7d767afdbc00f6f0f737451a97f8b0fe2454
Jonathan Chen <jonathanchen@gatech.edu>
2018-09-22 23:55:51 -0400

    Initial commit

---
+++ scripts/flappy1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBirdController : MonoBehaviour {
+
+	float velocity = 0;
+
+	void Start() {
+		
+	}
+	
+	void Update() {
+		transform.position += Vector3.up * velocity;
+		velocity -= 0.005f;
+		if(Input.GetKeyDown(KeyCode.Space)) {
+			velocity = 0.15f;
+		}
+	}
+}
